ef5d02f7e666a10d9f97f4e629073cbded6987a2
sthancock <steven.hancock@ed.ac.uk>
2026-03-04 11:40:21 +0000

    Starter code for sorter added,.

---
+++ week2/binary_search/searchObject.py
@@ -0,0 +1,27 @@
+
+'''
+A class to build on 
+and produce a binary
+search algorithm
+'''
+
+import numpy as np
+from sys import exit
+
+
+#####################################
+
+class dataSorter(object):
+  '''A class to hold data and provide examples of search algorithms'''
+
+  def __init__(self,numb=100):
+    '''Class initialiser'''
+    self.numb=numb
+    self.arr=
+
+  def sortArray(self):
+    '''Sort an array of data'''
+
+
+    return
+
