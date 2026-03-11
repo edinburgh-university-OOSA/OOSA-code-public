4cf7ea57f626fb06916c178b0532602010464896
sthancock <steven.hancock@ed.ac.uk>
2026-03-11 09:53:26 +0000

    Raster file updated.

--- week3/reproject/rasterExample.py
+++ week3/reproject/rasterExample.py
@@ -12,7 +12,7 @@ from osgeo.gdal import Warp
 if __name__=="__main__":
   '''Main'''
   # set input/output (could be done on command line)
-  inName='/geos/netdata/oosa/week3/raster/discRefl.LT.refl.tif'
+  inName='/geos/netdata/oosa/week3/sentinel-2/L2A_T30VVH_A020126_20210112T113447_2021-01-12/T30VVH_A020126_20210112T113447_B08.jp2'
   outName='warped.tif'
   # EPSG to project oo
   outEPSG='4326'
